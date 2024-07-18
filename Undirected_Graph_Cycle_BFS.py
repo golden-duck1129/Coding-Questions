5a5d41a8d42dc4d8cd60449efad17e765bf52fae
golden-duck1129 <pragyamishrapm1129@gmail.com>
2024-07-18 11:48:03 +0530

    Add files via upload

---
+++ Undirected_Graph_Cycle_BFS.py
@@ -0,0 +1,40 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def isCycle(self, V: int, adj: List[List[int]]) -> bool:
+        visited = [False]*V
+        for i in range(V):
+            if not visited[i]:
+                queue = deque([(i, -1)])
+                visited[i] = True
+
+                while queue:
+                    node, parent = queue.popleft()
+
+                    for neighbour in adj[node]:
+                        if not visited[neighbour]:
+                            queue.append(neighbour, node)
+                        elif neighbour != parent:
+                            return True
+
+        return False
+
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        V, E = map(int, input().split())
+        adj = [[] for _ in range(V)]
+
+        for _ in range(E):
+            u, v = map(int, input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+
+            obj = Solution()
+            if obj.isCycle(V, adj):
+                print("1")
+            else:
+                print("0")
